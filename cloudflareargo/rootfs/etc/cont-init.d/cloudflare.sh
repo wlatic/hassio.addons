d043c83375e9adc8166d318e07da5effe847868f
wlatic <jamesoh@gmail.com>
2020-01-16 18:56:00 -0500

    Update cloudflare.sh

--- cloudflareargo/rootfs/etc/cont-init.d/cloudflare.sh
+++ cloudflareargo/rootfs/etc/cont-init.d/cloudflare.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/with-contenv bashio
 declare hostname
-declare haurl
+declare url
 declare hostname2
-declare haurl2
+declare url2
 declare hostname3
-declare haur3
+declare ur3
 
 hostname=$(bashio::config 'hostname')
-haurl=$(bashio::config 'haurl')
+haurl=$(bashio::config 'url')
 hostname2=$(bashio::config 'hostname2')
-haurl2=$(bashio::config 'haurl2')
+haurl2=$(bashio::config 'url2')
 hostname3=$(bashio::config 'hostname3')
-haurl3=$(bashio::config 'haurl3')
+haurl3=$(bashio::config 'url3')
  
-echo -e "hostname: ${hostname}\nurl: ${haurl}" > /etc/cloudflared/1.yml
+echo -e "hostname: ${hostname}\nurl: $url}" > /etc/cloudflared/1.yml
 
 if bashio::config.has_value 'hostname2'; then
-    echo -e "hostname: ${hostname2}\nurl: ${haurl2}" > /etc/cloudflared/2.yml
+    echo -e "hostname: ${hostname2}\nurl: ${url2}" > /etc/cloudflared/2.yml
 fi
 
 if bashio::config.has_value 'hostname3'; then
-    echo -e "hostname: ${hostname3}\nurl: ${haurl3}" > /etc/cloudflared/3.yml
+    echo -e "hostname: ${hostname3}\nurl: ${url3}" > /etc/cloudflared/3.yml
 fi
