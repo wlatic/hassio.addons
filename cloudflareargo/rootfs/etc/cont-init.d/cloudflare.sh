008b269d7e78a7797d9d47b0b2f7c99889c616ee
wlatic <jamesoh@gmail.com>
2020-01-16 04:33:57 -0500

    Update cloudflare.sh

--- cloudflareargo/rootfs/etc/cont-init.d/cloudflare.sh
+++ cloudflareargo/rootfs/etc/cont-init.d/cloudflare.sh
@@ -4,5 +4,18 @@ declare haurl
 
 hostname=$(bashio::config 'hostname')
 haurl=$(bashio::config 'haurl')
+hostname2=$(bashio::config 'hostname2')
+haurl2=$(bashio::config 'haurl2')
+hostname3=$(bashio::config 'hostname3')
+haurl3=$(bashio::config 'haurl3')
 
-echo -e "hostname: ${hostname}\nurl: ${haurl}" > /etc/cloudflared/config.yml
+
+echo -e "hostname: ${hostname}\nurl: ${haurl}" > /etc/cloudflared/1.yml
+
+if bashio::config.has_value 'hostname2'; then
+    echo -e "hostname: ${hostname2}\nurl: ${haurl2}" > /etc/cloudflared/2.yml
+fi
+
+if bashio::config.has_value 'hostname3'; then
+    echo -e "hostname: ${hostname3}\nurl: ${haurl3}" > /etc/cloudflared/3.yml
+fi
