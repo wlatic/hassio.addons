a2bcd948f2eecf0a22403efcb7c3d58e526d7cc8
wlatic <jamesoh@gmail.com>
2020-01-16 20:51:59 -0500

    Update cloudflare.sh

--- cloudflareargo/rootfs/etc/cont-init.d/cloudflare.sh
+++ cloudflareargo/rootfs/etc/cont-init.d/cloudflare.sh
@@ -13,7 +13,7 @@ url2=$(bashio::config 'url2')
 hostname3=$(bashio::config 'hostname3')
 url3=$(bashio::config 'url3')
  
-echo -e "hostname: ${hostname}\nurl: $url}" > /etc/cloudflared/1.yml
+echo -e "hostname: ${hostname}\nurl: ${url}" > /etc/cloudflared/1.yml
 
 if bashio::config.has_value 'hostname2'; then
     echo -e "hostname: ${hostname2}\nurl: ${url2}" > /etc/cloudflared/2.yml
