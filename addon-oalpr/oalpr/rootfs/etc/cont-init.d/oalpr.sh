e69ae6dc9e60b41a24dab1e8e1e12df958c5cc7d
wlatic <jamesoh@gmail.com>
2020-12-01 20:58:45 -0500

    Update oalpr.sh

--- addon-oalpr/oalpr/rootfs/etc/cont-init.d/oalpr.sh
+++ addon-oalpr/oalpr/rootfs/etc/cont-init.d/oalpr.sh
@@ -7,11 +7,11 @@ declare port
 
 # Ensure configuration exists
 if ! bashio::fs.directory_exists '/config/oalpr/'; then
-    mkdir -p /config/oalpr/scripts \
+    mkdir -p /config/oalpr/scripts && \
+    mkdir -p /config/oalpr/log && \
+    mkdir -p /config/oalpr/data \
         || bashio::exit.nok "Failed to create node-red configuration directory"
 
     # Copy in template files
     cp /root/oalpr/scripts/oalpr.py /config/oalpr/scripts/
-    ln -s /root/oalpr/log /config/oalpr
-    ln -s /root/oalpr/data /config/oalpr
 fi
