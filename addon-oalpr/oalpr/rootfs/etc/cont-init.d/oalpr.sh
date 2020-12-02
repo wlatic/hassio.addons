367b986eab22146dcefc5138ad62b073ed4ae0e7
wlatic <jamesoh@gmail.com>
2020-12-01 20:28:00 -0500

    Create oalpr.sh

---
+++ addon-oalpr/oalpr/rootfs/etc/cont-init.d/oalpr.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Home Assistant Custom Add-on: Open ALPR
+# Configures OPEN ALPR before running
+# ==============================================================================
+declare port
+
+# Ensure configuration exists
+if ! bashio::fs.directory_exists '/config/oalpr/'; then
+    mkdir -p /config/oalpr/data && \
+    mkdir -p /config/oalpr/scripts && \
+    mkdir -p /config/oalpr/logs \
+        || bashio::exit.nok "Failed to create node-red configuration directory"
+
+    # Copy in template files
+    cp /root/oalpr/scripts/oalpr.py /config/oalpr/
+    ln -s /root/oalpr/logs /config/oalpr/logs
+    ln -s /root/oalpr/data /config/oalpr/data
+
+fi
