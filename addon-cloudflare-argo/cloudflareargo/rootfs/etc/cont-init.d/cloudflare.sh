c5b7a5e00e294861a404adc54944af8901e00a82
wlatic <jamesoh@gmail.com>
2021-06-02 00:39:05 -0400

    Update cloudflare.sh

--- addon-cloudflare-argo/cloudflareargo/rootfs/etc/cont-init.d/cloudflare.sh
+++ addon-cloudflare-argo/cloudflareargo/rootfs/etc/cont-init.d/cloudflare.sh
@@ -1,24 +1,9 @@
 #!/usr/bin/env bashio
-declare hostname
-declare url
-declare hostname2
-declare url2
-declare hostname3
-declare ur3
+declare -a options
+declare certificate
+certificate=$(bashio::config 'certificate')
+cf-cred=$(bashio::config 'cf-cred')
 
-hostname=$(bashio::config 'hostname')
-url=$(bashio::config 'url')
-hostname2=$(bashio::config 'hostname2')
-url2=$(bashio::config 'url2')
-hostname3=$(bashio::config 'hostname3')
-url3=$(bashio::config 'url3')
- 
-echo -e "hostname: ${hostname}\nurl: ${url}" > /etc/cloudflared/1.yml
-
-if bashio::config.has_value 'hostname2'; then
-    echo -e "hostname: ${hostname2}\nurl: ${url2}" > /etc/cloudflared/2.yml
-fi
-
-if bashio::config.has_value 'hostname3'; then
-    echo -e "hostname: ${hostname3}\nurl: ${url3}" > /etc/cloudflared/3.yml
+if ! bashio::fs.file_exists '${cf-cred}'; then
+   /opt/cloudflared --origincert=${certificate} --cred-file=/config/cf-cred.json tunnel create homeassistant
 fi
