5500130e524d7b40e196e182c66a79e45234848e
wlatic <jamesoh@gmail.com>
2021-06-02 00:54:49 -0400

    Update cloudflare.sh

--- addon-cloudflare-argo/cloudflareargo/rootfs/etc/cont-init.d/cloudflare.sh
+++ addon-cloudflare-argo/cloudflareargo/rootfs/etc/cont-init.d/cloudflare.sh
@@ -5,5 +5,29 @@ certificate=$(bashio::config 'certificate')
 cf-cred=$(bashio::config 'cf-cred')
 
 if ! bashio::fs.file_exists '${cf-cred}'; then
-   /opt/cloudflared --origincert=${certificate} --cred-file=/config/cf-cred.json tunnel create homeassistant
+   /opt/cloudflared --origincert=${certificate} --cred-file=/config/cf-ha.yml tunnel create homeassistant
+fi
+
+declare hostname
+declare service
+declare hostname2
+declare service2
+declare hostname3
+declare service3
+
+hostname=$(bashio::config 'hostname')
+service=$(bashio::config 'service')
+hostname2=$(bashio::config 'hostname2')
+service2=$(bashio::config 'service2')
+hostname3=$(bashio::config 'hostname3')
+service3=$(bashio::config 'service3')
+
+echo -e "ingress:\n  - hostname: ${hostname}\n    service: ${service}" > $(cf-cred}
+
+if bashio::config.has_value 'hostname2'; then
+    echo -e "  - hostname: ${hostname2}\n    service: ${service2}" >> $(cf-cred}
+fi
+
+if bashio::config.has_value 'hostname3'; then
+    echo -e "  - hostname: ${hostname3}\n    service: ${service3}" >> $(cf-cred}
 fi
