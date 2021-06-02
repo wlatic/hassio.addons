411ca6bd53bba84028161086a805847b02ca83bc
wlatic <jamesoh@gmail.com>
2021-06-02 04:15:36 -0400

    Update cloudflare.sh

--- addon-cloudflare-argo/cloudflareargo/rootfs/etc/cont-init.d/cloudflare.sh
+++ addon-cloudflare-argo/cloudflareargo/rootfs/etc/cont-init.d/cloudflare.sh
@@ -4,10 +4,10 @@ declare certificate
 declare cfcred
 
 certificate=$(bashio::config 'certificate')
-cfcred=$(bashio::config 'cfcred')
+cfcred=$(bashio::config 'cfconfig')
 
-if ! bashio::fs.file_exists "${cfcred}"; then
-/opt/cloudflared --origincert=${certificate} --cred-file=${cfcred} tunnel create homeassistant
+if ! bashio::fs.file_exists "/config/cf-ha.json"; then
+/opt/cloudflared --origincert=${certificate} --cred-file=/config/cf-ha.json tunnel create homeassistant
 fi
 
 declare hostname
@@ -24,14 +24,12 @@ service2=$(bashio::config 'service2')
 hostname3=$(bashio::config 'hostname3')
 service3=$(bashio::config 'service3')
 
-echo -e "credentials-file: /config/cf-ha.yml\ningress:\n  - hostname: ${hostname}\n    service: ${service}" > ${cf-cred}
-
-echo -e "tunnel: homeassistant\ncredentials-file: ${cf-cred}\n\ningress:\n  - hostname: hause.homenet.house\n     service: http://192.168.5.55:8123\n" > ${cf-cred}
+echo -e "tunnel: homeassistant\ncredentials-file: /config/cf-ha.json\n\ningress:\n  - hostname: hause.homenet.house\n     service: http://192.168.5.55:8123\n" > ${cfconfig}
 
 if bashio::config.has_value 'hostname2'; then
-    echo -e "  - hostname: ${hostname2}\n    service: ${service2}\n" >> ${cf-cred}
+    echo -e "  - hostname: ${hostname2}\n    service: ${service2}\n" >> ${cfconfig}
 fi
 
 if bashio::config.has_value 'hostname3'; then
-    echo -e "  - hostname: ${hostname3}\n    service: ${service3}\n  - service: http_status:404" >> ${cf-cred}
+    echo -e "  - hostname: ${hostname3}\n    service: ${service3}\n  - service: http_status:404" >> ${cfconfig}
 fi
